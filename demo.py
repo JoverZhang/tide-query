df56ab1d328c0b265169686780af0322b3ea871a
JoverZhang <joverzh@gmail.com>
2020-06-30 17:49:25 +0800

    style: format code.

--- demo.py
+++ demo.py
@@ -4,20 +4,20 @@ import pandas as pd
 
 from tide_query import YouDaoQuery, TideQuery as Tq
 
-if __name__ == "__main__":
-    source_excel_name = '/home/jover/Documents/english.xlsx'  # 源excel路径
+SOURCE_EXCEL = '/xx/xx.xlsx'  # 源excel路径
+TARGET_EXCEL = '/xx/xx.xlsx'  # 目标excel路径 (建议不要与源excel为同一个文件)
 
+if __name__ == "__main__":
     result_list: List[YouDaoQuery.Result] = Tq(
         target=YouDaoQuery(  # 使用有道词典作为搜索目标
             lang=YouDaoQuery.Language.cn,  # 选择中英文翻译
             silent=False,  # 不关闭提示
         ),
-        query_list=pd.read_excel(source_excel_name)['单词'].tolist(),  # 将源excel的"单词"列作为搜索源
+        query_list=pd.read_excel(SOURCE_EXCEL)['单词'].tolist(),  # 将源excel的"单词"列作为搜索源
         interval=(1, 2)  # 请求间隔 1-2秒
     ).start()  # 开始查询
 
-    target_excel_name = '/home/jover/Documents/english.xlsx'  # 目标excel路径 (建议不要与源excel为同一个文件)
-    writer = pd.ExcelWriter(target_excel_name)  # 打开目标excel
+    writer = pd.ExcelWriter(TARGET_EXCEL)  # 打开目标excel
     pd.DataFrame(data={
         '单词': [i['source'] for i in result_list],  # 生成单词列
         '音标1': [i['symbol_us'] for i in result_list],  # 生成美式音标列
